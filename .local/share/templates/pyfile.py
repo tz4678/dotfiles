0f55d886a6e7fcd42f796023f612c39616156a9d
Sergey M <tz4678@gmail.com>
2020-07-17 23:12:58 +0300

    update pyfile.py

--- .local/share/templates/pyfile.py
+++ .local/share/templates/pyfile.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+import argparse
 import sys
 from typing import List, Union
 
 
 def main(argv: List[str]) -> Union[None, int, str]:
-    """ Main function. """
-
+    parser = argparse.ArgumentParser()
+    ...
+    args = parser.parse_args(argv)
+    ...
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
