efc5d6316195eaa25a0d137d0b49f81cbaefac85
Sergey M <tz4678@gmail.com>
2020-07-17 22:59:40 +0300

    add templates

---
+++ .local/share/templates/pyfile.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+from typing import List, Union
+
+
+def main(argv: List[str]) -> Union[None, int, str]:
+    """ Main function. """
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
