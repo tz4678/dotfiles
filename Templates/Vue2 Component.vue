cc3b507708d63b9a9d038f8b6e43cec57947459f
Sergey M <tz4678@gmail.com>
2020-08-16 16:25:01 +0300

    vue template

---
+++ Templates/Vue2 Component.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data: () => ({}),
+  computed: {},
+  methods: {},
+  mounted() {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
