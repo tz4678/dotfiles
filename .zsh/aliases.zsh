646ea01e88cd5ee8c8a3d53f625d066cb9c9d7d9
Sergey M <tz4678@gmail.com>
2021-02-19 07:22:44 +0300

    update zsh aliases and docker compose

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -4,8 +4,20 @@ alias reload-shell='exec $SHELL -l'
 alias edit-zshrc='$EDITOR ~/.zshrc && . ~/.zshrc'
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
-alias docker-clean=' \
-  docker container prune -f ; \
-  docker image prune -f ; \
-  docker network prune -f ; \
-  docker volume prune -f '
+# docker
+# stop all containers
+alias docker-stop='docker stop $(docker ps -a -q)'
+# remove all containers
+alias docker-rma='docker rm $(docker ps -a -q)'
+# remove all images
+alias docker-rmi='docker rmi $(docker images -a -q)'
+# remove all volumes
+alias docker-rmv='docker volume rm $(docker volume ls -q)'
+# remove all networks
+alias docker-rmn='docker network rm `docker network ls -q`'
+alias docker-clean-all='docker-stop; docker-rma; docker-rmi; docker-rmv; docker-rmn'
+alias docker-clean-unused='docker system prune --all --force --volumes' 
+alias compose='docker-compose'
+alias docker-logs='docker logs -f'
+alias compose-logs='compose logs -f'
+
