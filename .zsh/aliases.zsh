feec11050c115edafec7c3cb4247c098e4dd4355
tz4678 <tz4678@gmail.com>
2021-02-20 05:50:48 +0300

    Update aliases.zsh

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 # stop all containers
 alias docker-stop='docker stop $(docker ps -a -q)'
 # remove all containers
-alias docker-rma='docker rm $(docker ps -a -q)'
+alias docker-rmc='docker rm $(docker ps -a -q)'
 # remove all images
 alias docker-rmi='docker rmi $(docker images -a -q)'
 # remove all volumes
@@ -16,9 +16,8 @@ alias docker-rmv='docker volume rm $(docker volume ls -q)'
 # remove all networks
 alias docker-rmn='docker network rm `docker network ls -q`'
 # Нужно ли и сети удалять?
-alias docker-clean-all='docker-stop; docker-rma; docker-rmi; docker-rmv;'
+alias docker-clean-all='docker-stop; docker-rmc; docker-rmi; docker-rmv;'
 alias docker-clean-unused='docker system prune --all --force --volumes' 
 alias compose='docker-compose'
 alias docker-logs='docker logs -f'
 alias compose-logs='compose logs -f'
-
